b44ae9b176a70681a388973ba97d3dc015ae76d1
Dofo <aserranom29@gmail.com>
2025-08-07 01:08:54 -0600

    Solved puzzled 3

--- AI/knights/puzzle.py
+++ AI/knights/puzzle.py
@@ -16,6 +16,7 @@ CKnave = Symbol("C is a Knave")
 knowledge0 = And(
     Or(AKnight, AKnave),
     Not(And(AKnight, AKnave)),
+
     Implication(AKnight, And(AKnight, AKnave)),
     Implication(AKnave, Not(And(AKnight, AKnave)))
 )
@@ -28,6 +29,7 @@ knowledge1 = And(
     Not(And(AKnight, AKnave)),
     Or(BKnight, BKnave),
     Not(And(BKnight, BKnave)),
+
     Implication(AKnight, And(AKnave, BKnave)),
     Implication(AKnave, Not(And(AKnave, BKnave)))
 )
@@ -40,8 +42,10 @@ knowledge2 = And(
     Not(And(AKnight, AKnave)),
     Or(BKnight, BKnave),
     Not(And(BKnight, BKnave)),
+
     Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
     Implication(AKnave, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
+
     Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
     Implication(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, BKnight)))),
 )
@@ -58,12 +62,18 @@ knowledge3 = And(
     Not(And(BKnight, BKnave)),
     Or(CKnight, CKnave),
     Not(And(CKnight, CKnave)),
+
+    Implication(AKnight, Or(AKnight, AKnave)),
+    Implication(AKnave, Not(Or(AKnight, AKnave))),
+
     Implication(BKnight, Implication(AKnight, BKnave)),
-    Implication(BKnave, Not(Implication(AKnight, BKnave))),
+    Implication(BKnave, Implication(AKnave, Not(BKnave))),
+
     Implication(BKnight, CKnave),
-    Implication(BKnave, Not(CKnave)),
+    Implication(BKnave, CKnight),
+
     Implication(CKnight, AKnight),
-    Implication(CKnave, Not(AKnight)),
+    Implication(CKnave, AKnave),
 )
 
 # fmt: on
